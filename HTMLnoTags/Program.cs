b95ecb0c18c2119804f00caac40bfc09c76b1dfd
Martin Pashov <80414148+Martin-Pashov@users.noreply.github.com>
2021-03-29 22:56:39 +0300

    Add project files.

---
+++ HTMLnoTags/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace HTMLnoTags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a HTML with tags: ");
+            string content = Console.ReadLine();
+
+            string pattern = @"<title>([a-z ]*)</title>";
+            Regex regX = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            Match match = regX.Match(content);
+            Console.WriteLine($"Title: {match.Groups[1].Value}");
+            content = Regex.Replace(content, pattern, "");
+
+            pattern = @"<[^>]*>";
+
+            content = Regex.Replace(content, pattern, "");
+
+            Console.WriteLine("Body:");
+
+            Console.WriteLine(content);
+        }
+    }
+}
